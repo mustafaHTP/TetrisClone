151fe880ee23c4b6513a00cccb9f3e37bc40c25e
Mustafa Hatipoğlu <56385804+mustafaHTP@users.noreply.github.com>
2024-03-03 13:51:53 +0300

    feat: add sound volume options to pause menu

--- Assets/Scripts/UI/CanvasPause.cs
+++ Assets/Scripts/UI/CanvasPause.cs
@@ -1,25 +1,51 @@
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CanvasPause : MonoBehaviour
 {
-    public void Continue()
-    {
-        GetComponent<CanvasGroup>().interactable = false;
-        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    [SerializeField] private AudioMixer _audioMixer;
+    [SerializeField] private Slider _musicVolumeSlider;
+    [SerializeField] private Slider _sfxVolumeSlider;
 
-        GetComponent<Transform>().DOScale(0f, 1f).SetUpdate(true);
-        GetComponent<CanvasGroup>().DOFade(0f, 1f).SetUpdate(true).OnComplete(() =>
-        {
-            Time.timeScale = 1f;
-            gameObject.SetActive(false);
-        });
+    public void OnChangeMusicVolume()
+    {
+        //Audio mixer volume has logarithmic scale
+        float newVolume = Mathf.Log10(_musicVolumeSlider.value) * 20f;
+        _audioMixer.SetFloat(Utilities.AudioMixerParams.MusicVolume, newVolume);
+    }
+
+    public void OnChangeSFXVolume()
+    {
+        //Audio mixer volume has logarithmic scale
+        float newVolume = Mathf.Log10(_sfxVolumeSlider.value) * 20f;
+        _audioMixer.SetFloat(Utilities.AudioMixerParams.SFXVolume, newVolume);
     }
 
     public void GoMainMenu()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(Utilities.SceneName.MainMenu);
+    }
+
+    private void InitializeOptionsMenu()
+    {
+        //Set slider values
+        //Music
+        _audioMixer.GetFloat(Utilities.AudioMixerParams.MusicVolume, out float currentVolume);
+        currentVolume = Mathf.Pow(10f, currentVolume / 20f);
+        _musicVolumeSlider.value = currentVolume;
+
+        //SFX
+        _audioMixer.GetFloat(Utilities.AudioMixerParams.SFXVolume, out currentVolume);
+        currentVolume = Mathf.Pow(10f, currentVolume / 20f);
+        _sfxVolumeSlider.value = currentVolume;
+    }
+
+    private void OnEnable()
+    {
+        InitializeOptionsMenu();
     }
 }
